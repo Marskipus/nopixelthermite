54ceb1ccfc09f867d3207d3790aaf30e18a547b0
James Driscoll <thejamesdriscoll@gmail.com>
2021-12-15 16:44:05 -0600

    created selectboxes fucntion which highlights random boxes and takes note of which ones were chosen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let size = 6
 let maxWidth = 600
+let allBoxes = []
 function makeBlocks() {
+
     for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
         row.className = "row";
@@ -9,6 +11,7 @@ function makeBlocks() {
             let box = document.createElement('div');
             box.className = `box-${i}${j}`
             box.classList.add('box')
+            allBoxes.push(`box-${i}${j}`)
             row.appendChild(box);
             box.addEventListener('click',selectBox)
         }
@@ -20,8 +23,22 @@ function makeBlocks() {
             boxes[k].style.height = maxWidth / size + "px";
         }
     }
+    console.log(allBoxes)
 }
 makeBlocks()
 function selectBox(){
 
 }
+function random(num){
+    return Math.floor(Math.random()*num)
+}
+
+function selectBoxes(difficulty){
+    let selectedBoxes = []
+    for(let i = 0; i < difficulty; i++){
+        selectedBoxes.push(allBoxes[random(allBoxes.length)])
+        document.querySelector(`.${selectedBoxes[i]}`).classList.add('selected')
+        allBoxes.splice(i,1)
+    }
+}
+selectBoxes(10)
