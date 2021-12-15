6c4fb357e0c9cd2913ae0d9f6d1af0a302204ac5
James Driscoll <thejamesdriscoll@gmail.com>
2021-12-15 17:13:12 -0600

    added event listeners to track if box clicked was selected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let size = 6
 let maxWidth = 600
 let allBoxes = []
+let unchangedAllBoxes = []
 function makeBlocks() {
 
     for (let i = 0; i < size; i++) {
@@ -23,8 +24,12 @@ function makeBlocks() {
             boxes[k].style.height = maxWidth / size + "px";
         }
     }
-    console.log(allBoxes)
+    return [allBoxes]
 }
+
+//since boxes are removed from allBoxes if they are selected, i need an unchanged array to add the event listeners
+unchangedAllBoxes = allBoxes
+
 makeBlocks()
 function selectBox(){
 
@@ -32,13 +37,32 @@ function selectBox(){
 function random(num){
     return Math.floor(Math.random()*num)
 }
-
+let selectedBoxes = []
 function selectBoxes(difficulty){
-    let selectedBoxes = []
+
     for(let i = 0; i < difficulty; i++){
         selectedBoxes.push(allBoxes[random(allBoxes.length)])
         document.querySelector(`.${selectedBoxes[i]}`).classList.add('selected')
         allBoxes.splice(i,1)
     }
+    return selectedBoxes
 }
 selectBoxes(10)
+
+function classes() {
+    console.log(unchangedAllBoxes)
+    for(let i = 0; i < unchangedAllBoxes.length; i++) {
+        document.querySelector(`.${unchangedAllBoxes[i]}`).addEventListener('click', () => {
+            if (document.querySelector(`.${unchangedAllBoxes[i]}`).classList.contains('selected')) {
+                console.log('this was selected')
+            } else {
+                console.log('this was not selected')
+            }
+        })
+    }
+}
+classes()
+
+
+
+
