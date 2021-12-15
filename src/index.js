84a016211e99c49f1648d1f503fa4bc9dcfdc97e
James Driscoll <thejamesdriscoll@gmail.com>
2021-12-15 16:19:34 -0600

    created 6x6 square and centered it

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-console.log('hello webpack')
-console.log('hello')
+let size = 6
+let maxWidth = 600
+function makeBlocks() {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.className = "row";
+
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement('div');
+            box.className = "box";
+            row.appendChild(box);
+            box.addEventListener('click',selectBox)
+        }
+        document.getElementById('grid-container').appendChild(row);
+
+        var boxes = document.getElementsByClassName("box");
+        for (let k = 0; k < boxes.length; k++) {
+            boxes[k].style.width = maxWidth / size + "px";
+            boxes[k].style.height = maxWidth / size + "px";
+        }
+    }
+}
+makeBlocks()
+function selectBox(){
+
+}
