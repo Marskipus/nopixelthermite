00c09f0b4fe94376aa733b9dc41866b3ca059a88
James Driscoll <thejamesdriscoll@gmail.com>
2021-12-16 14:46:05 -0600

    created check function to announce winner when all boxes are clicked

--- src/index.js
+++ src/index.js
@@ -1,88 +1,124 @@
-let size = 6
-let maxWidth = 600
-let allBoxes = []
-let unchangedAllBoxes = []
-const startBtn = document.querySelector('[data-start-button]')
-const gridContainer = document.querySelector('[data-grid-container]')
-startBtn.addEventListener('click', playRound)
+document.querySelector('[data-start-button]').addEventListener('click', test)
 
-function playRound() {
-    //reset everything and then generate a board
-    allBoxes = []
-    unchangedAllBoxes = []
-    selectedBoxes = []
-    gridContainer.textContent = ""
-    makeBlocks()
-    selectBoxes(10)
-    classes()
-}
+function test() {
+    let size = 6
+    let maxWidth = 600
+    let allBoxes = []
+    let unchangedAllBoxes = []
+    let uniqueArray = []
+    let selectedBoxes = []
+    const log = console.log
+    const startBtn = document.querySelector('[data-start-button]')
+    const gridContainer = document.querySelector('[data-grid-container]')
+    startBtn.addEventListener('click', playRound)
 
-
-function makeBlocks() {
-
-    for (let i = 0; i < size; i++) {
-        let row = document.createElement('div');
-        row.className = "row";
-
-        for (let j = 0; j < size; j++) {
-            let box = document.createElement('div');
-            box.className = `box-${i}${j}`
-            box.classList.add('box')
-            allBoxes.push(`box-${i}${j}`)
-            unchangedAllBoxes.push(`box-${i}${j}`)
-            row.appendChild(box);
-            box.addEventListener('click', selectBox)
-        }
-        document.getElementById('grid-container').appendChild(row);
-
-        var boxes = document.getElementsByClassName("box");
-        for (let k = 0; k < boxes.length; k++) {
-            boxes[k].style.width = maxWidth / size + "px";
-            boxes[k].style.height = maxWidth / size + "px";
-        }
+    function playRound() {
+        //reset everything and then generate a board
+        allBoxes = []
+        unchangedAllBoxes = []
+        selectedBoxes = []
+        uniqueArray = []
+        gridContainer.textContent = ""
+        makeBlocks()
+        selectBoxes(10)
+        classes()
+        check()
     }
-    return [allBoxes]
-}
 
-function selectBox() {
+    function makeBlocks() {
 
-}
+        for (let i = 0; i < size; i++) {
+            let row = document.createElement('div');
+            row.className = "row";
 
-function random(num) {
-    return Math.floor(Math.random() * num)
-}
-let selectedBoxes = []
-
-function selectBoxes(difficulty) {
-
-    for (let i = 0; i < difficulty; i++) {
-        selectedBoxes.push(allBoxes[random(allBoxes.length)])
-        document.querySelector(`.${selectedBoxes[i]}`).classList.add('selected')
-        allBoxes.splice(i, 1)
-    }
-    return selectedBoxes
-}
-
-function classes() {
-    console.log(selectedBoxes)
-    for (let i = 0; i < unchangedAllBoxes.length; i++) {
-        let thebox = document.querySelector(`.${unchangedAllBoxes[i]}`)
-        document.querySelector(`.${unchangedAllBoxes[i]}`).addEventListener('click', () => {
-            
-            if (thebox.classList.contains('selected')) {
-            console.log('this was selected')
-            choose(thebox)
-            } else {
-                console.log('this was not selected')
-                wrongChoice(thebox)
+            for (let j = 0; j < size; j++) {
+                let box = document.createElement('div');
+                box.className = `box-${i}${j}`
+                box.classList.add('box')
+                allBoxes.push(`box-${i}${j}`)
+                unchangedAllBoxes.push(`box-${i}${j}`)
+                row.appendChild(box);
+                box.addEventListener('click', selectBox)
             }
-        })
-    }
-}
-function choose(el) {
-    el.classList.add("selected1");
+            document.getElementById('grid-container').appendChild(row);
 
+            var boxes = document.getElementsByClassName("box");
+            for (let k = 0; k < boxes.length; k++) {
+                boxes[k].style.width = maxWidth / size + "px";
+                boxes[k].style.height = maxWidth / size + "px";
+            }
+        }
+        //console.log(allBoxes)
+        return allBoxes
+    }
+    //allBoxes = makeBlocks()
+
+    function selectBox() {
+
+    }
+
+    function random(num) {
+        return Math.floor(Math.random() * num)
+    }
+
+
+    function selectBoxes(difficulty) {
+
+        for (let i = 0; i < difficulty; i++) {
+            selectedBoxes.push(allBoxes[random(allBoxes.length)])
+
+            document.querySelector(`.${selectedBoxes[i]}`).classList.add('selected')
+            allBoxes.splice(i, 1)
+
+        }
+        return selectedBoxes
+    }
+
+    //remove duplicates in selectedBoxes
+    uniqueArray = [...new Set(selectedBoxes)]
+
+    function classes() {
+        log(unchangedAllBoxes)
+        for (let i = 0; i < unchangedAllBoxes.length; i++) {
+            let thebox = document.querySelector(`.${unchangedAllBoxes[i]}`)
+            document.querySelector(`.${unchangedAllBoxes[i]}`).addEventListener('click', () => {
+                if (thebox.classList.contains('selected')) {
+                    console.log('this was selected')
+                    choose(thebox)
+                    check()
+                } else if (thebox.classList.contains('selected1')) {
+                    console.log('this was already selected')
+                } else {
+                    console.log('this was not selected')
+                    wrongChoice(thebox)
+                }
+            })
+        }
+
+
+        //return uniqueArray
+    }
+
+    function choose(el) {
+        el.classList.add("selected1");
+        el.classList.remove('selected')
+    }
+
+    function wrongChoice(el) {
+        el.classList.add('wrongchoice')
+    }
+    
+
+    function check() {
+        //checks the classes on all boxes, returns if one still has the selected class. and announces winner if it can finish the loop
+        for (let i = 0; i < unchangedAllBoxes.length; i++) {
+            if (document.querySelector(`.${unchangedAllBoxes[i]}`).classList.contains('selected')) {
+                return false
+            }
+        }
+        console.log('winner')
+    }
+    
 }
-function wrongChoice(el){
-    el.classList.add('wrongchoice')
-}
+
+test()
